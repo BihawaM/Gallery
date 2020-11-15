06f927ad4a884d0413aaece45f2c16d7334a0faa
BihawaM <bihawam271@gmail.com>
2020-11-15 19:58:08 +0300

    add location class

--- gallery/models.py
+++ gallery/models.py
@@ -14,17 +14,17 @@ class categories(models.Model):
     def __str__(self):
         return self.name
 
-# class Location(models.Model):
-#     name = models.CharField(max_length=30)
+class Location(models.Model):
+    name = models.CharField(max_length=30)
 
-#     def save_location(self):
-#         self.save()
+    def save_location(self):
+        self.save()
 
-#     def delete_location(self):
-#         self.delete()
+    def delete_location(self):
+        self.delete()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 # class Image(models.Model):
 #     title = models.CharField(max_length=30,default="title")
