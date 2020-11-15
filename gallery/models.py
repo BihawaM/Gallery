eaa32fe7149c4b412197ad45153ca19e8b385534
BihawaM <bihawam271@gmail.com>
2020-11-15 19:56:53 +0300

    add categories class

--- gallery/models.py
+++ gallery/models.py
@@ -1,18 +1,18 @@
-# from django.db import models
-# import cloudinary
+from django.db import models
+import cloudinary
 
-# # Create your models here.
-# class categories(models.Model):
-#     name = models.CharField(max_length=30)
+# Create your models here.
+class categories(models.Model):
+    name = models.CharField(max_length=30)
 
-#     def save_category(self):
-#         self.save()
+    def save_category(self):
+        self.save()
 
-#     def delete_category(self):
-#         self.delete()
+    def delete_category(self):
+        self.delete()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 # class Location(models.Model):
 #     name = models.CharField(max_length=30)
