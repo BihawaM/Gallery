e05748289e90ad8064c79621b5dcbc04be44e417
BihawaM <bihawam271@gmail.com>
2020-11-15 20:01:29 +0300

    add ImageAdmin class

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,9 +1,9 @@
-# from django.contrib import admin
-# from .models import Image,Location,categories
+from django.contrib import admin
+from .models import Image,Location,categories
 
-# # Register your models here.
-# class ImageAdmin(admin.ModelAdmin):
-#     filter_horizontal=("categories",)
+# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal=("categories",)
 
 
 # admin.site.register(Image,ImageAdmin),
