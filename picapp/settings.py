8c3b5223b561eeb486fc2acd6dd2784127e3337c
BihawaM <bihawam271@gmail.com>
2020-11-16 11:30:25 +0300

    deployment

--- picapp/settings.py
+++ picapp/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
